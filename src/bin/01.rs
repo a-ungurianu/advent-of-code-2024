de477a0e9324ccbef6553d19af3fb6f3f4781f4a
Alex Ungurianu <alex.ungurianu@protonmail.com>
2024-12-02 09:58:25 +0000

    day 1: part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (mut left_list, mut right_list): (Vec<_>, Vec<_>) = input
+fn parse_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
         .lines()
         .map(|line| {
             let mut numbers = line
@@ -9,7 +11,12 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .map(|chunk| chunk.parse::<u32>().unwrap());
             (numbers.next().unwrap(), numbers.next().unwrap())
         })
-        .unzip();
+        .unzip()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut left_list, mut right_list): (Vec<_>, Vec<_>) = parse_lists(input);
+
     left_list.sort();
     right_list.sort();
 
@@ -26,7 +33,17 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (left_list, right_list): (Vec<_>, Vec<_>) = parse_lists(input);
+
+    let mut frequency_table: HashMap<u32, u32> = HashMap::new();
+
+    for &item in &right_list {
+        *frequency_table.entry(item).or_insert(0) += 1;
+    }
+
+    Some(left_list.into_iter().map(|val|{
+        val * *frequency_table.entry(val).or_insert(0)
+    }).sum())
 }
 
 #[cfg(test)]
@@ -36,12 +53,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
