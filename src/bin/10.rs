01730ebc701f5066d68b6c85815a5f6f46740205
Alex Ungurianu <alex.ungurianu@protonmail.com>
2024-12-10 21:14:51 +0000

    day 10: part 1 - wrong -  count paths instead of distinct heads

---
+++ src/bin/10.rs
@@ -0,0 +1,106 @@
+use advent_of_code::Point;
+
+advent_of_code::solution!(10);
+
+const DIRS: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let map: Vec<_> = input.lines().collect();
+    let mut path_count: Vec<_> = map
+        .iter()
+        .map(|row| vec![0 as u32; row.as_bytes().len()])
+        .collect();
+
+    for (row_idx, row) in path_count.iter_mut().enumerate() {
+        for (col_idx, c) in row.iter_mut().enumerate() {
+            if map[&Point(row_idx as i32, col_idx as i32)] == b'9' {
+                *c = 1
+            }
+        }
+    }
+
+    for s in (b'0'..=b'8').rev() {
+        for (row_idx, row) in map.iter().enumerate() {
+            for (col_idx, _) in row.as_bytes().iter().enumerate() {
+                let p = Point(row_idx as i32, col_idx as i32);
+                if map[&p] == s {
+                    let count = DIRS
+                        .map(|dir| &p + dir)
+                        .iter()
+                        .filter(|p| {
+                            0 <= p.0
+                                && p.0 < path_count.len() as i32
+                                && 0 <= p.1
+                                && p.1 < path_count[0].len() as i32
+                        })
+                        .map(|p| {
+                            if map[p] == s + 1 {
+                                path_count[p.0 as usize][p.1 as usize]
+                            } else {
+                                0
+                            }
+                        })
+                        .sum();
+                    path_count[row_idx][col_idx] = count;
+                }
+            }
+        }
+    }
+
+    let mut res = 0;
+    for (row_idx, row) in map.iter().enumerate() {
+        for (col_idx, _) in row.as_bytes().iter().enumerate() {
+            let p = Point(row_idx as i32, col_idx as i32);
+            if map[&p] == b'0' {
+                res += path_count[p.0 as usize][p.1 as usize];
+            }
+        }
+    }
+    Some(res)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(36));
+    }
+
+    #[test]
+    fn test_part_one_simple() {
+        let result = part_one(
+            "0123
+1234
+8765
+9876",
+        );
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn test_part_one_knots() {
+        let result = part_one(
+            "2290229
+2221298
+2222227
+6543456
+7652987
+8762222
+9872222",
+        );
+        assert_eq!(result, Some(4));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
 pub mod template;
 
 #[derive(Debug)]
-pub struct Point(pub usize, pub usize);
+pub struct Point(pub i32, pub i32);
 
 impl std::ops::Add<(i32, i32)> for &Point {
     type Output = Point;
 
     fn add(self, rhs: (i32, i32)) -> Self::Output {
-        Point(
-            (self.0 as i32 + rhs.0) as usize,
-            (self.1 as i32 + rhs.1) as usize,
-        )
+        Point(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
 
@@ -20,6 +17,6 @@ impl std::ops::Index<&Point> for Grid<'_> {
     type Output = u8;
 
     fn index(&self, index: &Point) -> &Self::Output {
-        &self[index.0].as_bytes()[index.1]
+        &self[index.0 as usize].as_bytes()[index.1 as usize]
     }
 }
