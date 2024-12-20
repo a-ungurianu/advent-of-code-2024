eb4fb3133277f09402d3fd688d79a24d2b062716
Alex Ungurianu <alex.ungurianu@protonmail.com>
2024-12-20 01:38:13 +0000

    day 18: part 2

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     hash::Hash,
+    ops::RangeBounds,
 };
 
 use advent_of_code::Point;
@@ -10,11 +11,35 @@ advent_of_code::solution!(18);
 fn in_bounds(point: &Point, n: u32) -> bool {
     return 0 <= point.0 && point.0 < n as i32 && 0 <= point.1 && point.1 < n as i32;
 }
-
-fn part_one_range(input: &str, n: u32, take: u32) -> u32 {
+fn steps_needed(points: &[Point], n: u32) -> Option<u32> {
     let dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    let mut points_hm: HashMap<_, _> = points.iter().map(|p| (*p, -1)).collect();
 
-    let mut points: HashMap<_, _> = input
+    let mut queue: VecDeque<Point> = VecDeque::new();
+    queue.push_back(Point(0, 0));
+    points_hm.insert(Point(0, 0), 0);
+
+    while !queue.is_empty() {
+        let c = queue.pop_front().unwrap();
+        if c == Point(n as i32 - 1, n as i32 - 1) {
+            return Some(*points_hm.get(&c).unwrap() as u32);
+        }
+
+        for dir in dirs {
+            let p = &c + dir;
+            if in_bounds(&p, n) {
+                if !points_hm.contains_key(&p) {
+                    let res = points_hm.get(&c).unwrap() + 1;
+                    points_hm.insert(p, res);
+                    queue.push_back(p);
+                }
+            }
+        }
+    }
+    None
+}
+fn part_one_range(input: &str, n: u32, take: u32) -> Option<u32> {
+    let points: Vec<_> = input
         .lines()
         .map(|s| {
             let [x, y] = s
@@ -27,41 +52,41 @@ fn part_one_range(input: &str, n: u32, take: u32) -> u32 {
 
             Point(x, y)
         })
-        .map(|p| (p, -1))
-        .take(take as usize)
         .collect();
 
-    let mut queue: VecDeque<Point> = VecDeque::new();
-    queue.push_back(Point(0, 0));
-    points.insert(Point(0, 0), 0);
+    steps_needed(&points[..take as usize], n)
+}
 
-    while !queue.is_empty() {
-        let c = queue.pop_front().unwrap();
-        if c == Point(n as i32 - 1, n as i32 - 1) {
-            return *points.get(&c).unwrap() as u32;
-        }
+fn part_two_range(input: &str, n: u32) -> Option<Point> {
+    let points: Vec<_> = input
+        .lines()
+        .map(|s| {
+            let [x, y] = s
+                .split(",")
+                .map(|p| p.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()[..]
+            else {
+                panic!("")
+            };
 
-        for dir in dirs {
-            let p = &c + dir;
-            if in_bounds(&p, n) {
-                if !points.contains_key(&p) {
-                    let res = points.get(&c).unwrap() + 1;
-                    points.insert(p, res);
-                    queue.push_back(p);
-                }
-            }
-        }
-    }
+            Point(x, y)
+        })
+        .collect();
 
-    0
+    let r = (0..=points.len())
+        .collect::<Vec<_>>()
+        .partition_point(|t| steps_needed(&points[..*t], n).is_some());
+
+    points.get(r - 1).map(|p| *p)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(part_one_range(input, 71, 1024))
+    part_one_range(input, 71, 1024)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let r = part_two_range(input, 71).unwrap();
+    Some(format!("{},{}", r.0, r.1))
 }
 
 #[cfg(test)]
@@ -71,12 +96,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one_range(&advent_of_code::template::read_file("examples", DAY), 7, 12);
-        assert_eq!(result, 22);
+        assert_eq!(result, Some(22));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two_range(&advent_of_code::template::read_file("examples", DAY), 7);
+        assert_eq!(result, Some(Point(6, 1)));
     }
 }
